2d5150896ada5f5e2a65ddc72a81c6d8da2b069d
geraldos <sepdwijaya18@gmail.com>
2020-07-15 16:48:03 +0700

    add ejs file

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
 
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: false,
-    })
+  express.urlencoded({
+    extended: false,
+  })
 );
 
-app.use('/api/users', require('./routes/users'))
+app.set("view engine", "ejs");
+app.use("/api/users", require("./routes/users"));
 
-const {
-    PORT
-} = require('./config')
-const {
-    db
-} = require('./config')
+const { PORT } = require("./config");
+const { db } = require("./config");
 
-app.get('/', (req, res) => {
-    res.send('Welcome to mongoose demo')
-})
+app.get("/", (req, res) => {
+  res.render("pages/home.ejs");
+});
 
 if (db) {
-    console.log(`Connected to database `)
-    app.listen(PORT, () => {
-        console.log(`Server runs on your port ${PORT}`)
-    })
+  console.log(`Connected to database `);
+  app.listen(PORT, () => {
+    console.log(`Server runs on your port ${PORT}`);
+  });
 }
